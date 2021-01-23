874348ec4e51e745dafec6fe273c6d0204f015cf
atayl16 <atayl16@yahoo.com>
2021-01-23 13:17:17 +0100

    update stripe2

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
-Rails.configuration.stripe = {
-  publishable_key: Rails.application.credentials[Rails.env.to_sym][:stripe][:stripe_publishable],
-  secret_key: Rails.application.credentials[Rails.env.to_sym][:stripe][:stripe_secret]
-}
-Stripe.api_key = Rails.application.credentials[Rails.env.to_sym][:stripe][:secret]
+# Rails.configuration.stripe = {
+#   publishable_key: Rails.application.credentials[Rails.env.to_sym][:stripe][:stripe_publishable],
+#   secret_key: Rails.application.credentials[Rails.env.to_sym][:stripe][:stripe_secret]
+# }
+# Stripe.api_key = Rails.application.credentials[Rails.env.to_sym][:stripe][:secret]
