9cbb63bad4d24e9d9093b74937ee7891bcbcc1be
atayl16 <atayl16@yahoo.com>
2021-02-10 13:02:28 +0100

    update mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = {host: "http://atayl16-course-app.herokuapp.com/", protocol: "http"}
+  config.action_mailer.default_url_options = {host: "https://www.islam4kids.academy/", protocol: "https"}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
@@ -8,8 +8,8 @@ Rails.application.configure do
     email: {
       deliver_with: :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
       email_prefix: "[PREFIX] ",
-      sender_address: %("Islam4Kids error" <support@Islam4Kids.herokuapp.com>),
-      exception_recipients: %w[yshmarov@gmail.com]
+      sender_address: %("Islam4Kids error" <no-reply@Islam4Kids.academy>),
+      exception_recipients: %w[inspirebyexample0@gmail.com]
     }
   # Settings specified here will take precedence over those in config/application.rb.
 
