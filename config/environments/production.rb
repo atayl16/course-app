275992f1415b1f4d56d307ca219c038045b20295
atayl16 <atayl16@yahoo.com>
2021-02-10 14:50:31 +0100

    more sendgrid fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
    address: ‘smtp.sendgrid.net’,
    port: “25”,
