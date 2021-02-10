534c0e8bd795b03c94a2f0a35971b630392f56eb
atayl16 <atayl16@yahoo.com>
2021-02-10 14:30:56 +0100

    more sendgrid fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,16 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+   address: ‘smtp.sendgrid.net’,
+   port: “25”,
+   domain: ‘heroku.com’,
+   user_name: ENV[“SENDGRID_USERNAME”],
+   password: ENV[“SENDGRID_PASSWORD”],
+   authentication: ‘plain’,
+   enable_starttls_auto: true
+  }
 
   Rails.application.config.middleware.use ExceptionNotification::Rack,
     email: {
